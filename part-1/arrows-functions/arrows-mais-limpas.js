ac853eb643eb43dd700b2de310b8e2ccab9801c2
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2018-03-22 01:55:48 -0300

    Exemplos de arrows functions
    Exemplo de utilizacao de arrows functions.

---
+++ part-1/arrows-functions/arrows-mais-limpas.js
@@ -0,0 +1,3 @@
+const biggerThenFifty = [60, 10, 5, 100].map(values => values > 50);
+
+console.log(biggerThenFifty);
---
+++ part-1/arrows-functions/arrows.js
@@ -0,0 +1,5 @@
+const biggerThenFifty = [60, 10, 5, 100].map(values => {
+  return values > 50;
+});
+
+console.log(biggerThenFifty);
