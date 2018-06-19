26bbf337ce7b58019d79b49a4e92e164cbcfe312
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2018-06-18 23:33:16 -0300

    Chai.js
    Ajustes do capitulo para finalização.

---
+++ part-2/porque-utilizar/Asserts_Chai.js/Test.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+
+const sum = require('./App');
+
+// Bool
+assert.equal(true, true);
+assert.equal(false, false);
+
+// Int
+assert.equal(1, 1);
+assert.equal(0, 0);
+
+// Teste Error
+assert.equal(0, 1, 'Valor diferentes');
