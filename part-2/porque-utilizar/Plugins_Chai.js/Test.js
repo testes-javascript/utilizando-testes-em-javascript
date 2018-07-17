67022e0f637aef69a4b1ad4ec53048657ce4223e
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2018-07-16 22:58:34 -0300

    Finalizacao do tipo assert
    Finalizacao do tipo assert

--- part-2/porque-utilizar/Plugins_Chai.js/Test.js
+++ part-2/porque-utilizar/Plugins_Chai.js/Test.js
@@ -14,7 +14,35 @@ assert.equal(1, 1);
 assert.equal(0, 0);
 
 // Teste Error
-assert.equal(0, 1, 'Valor diferentes');
+try {
+  assert.equal(0, 1, 'Valor diferentes');
+} catch (error) {
+  console.log(error.message);
+}
 
 // Json
 expect('user.json').to.be.a.jsonFile();
+
+// Object
+jsonObj = {
+  _id: '5b2ee24639181674db82ced5',
+  isActive: false,
+  name: 'Foster Rowe',
+  company: 'ERSUM',
+  email: 'fosterrowe@ersum.com'
+};
+
+try {
+  expect('user.json').to.be.a.jsonFile().and.to.be.jsonObj(jsonObj);
+} catch (error) {
+  console.log('\n', error.message);
+}
+
+// Object
+try {
+  expect('repo.json').to.be.a
+    .jsonFile()
+    .and.contain.jsonWithProps({ repoName: 'douglas' });
+} catch (error) {
+  console.log('\n', error.message);
+}
