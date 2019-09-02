72f42c1264bf60ac9a33ef73e9a3e60ae10fb8be
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2019-09-02 14:24:02 -0300

    Ajuste do capitulo

--- part-4/MyLinter/node-app/.eslintrc.js
+++ part-4/MyLinter/node-app/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2019,
   },
+  extends: 'airbnb-base',
   plugins: ['my-eslint-rules'],
   rules: {
     'my-eslint-rules/async-func-name': 'error',
--- part-4/MyLinter/node-app/index.js
+++ part-4/MyLinter/node-app/index.js
@@ -1,18 +1,18 @@
-async function myFunctionAsync () {
+async function myFunctionAsync() {
   return 'Teste';
 }
 
-async function myTest () {
-  let tips = [
+async function myTest() {
+  const tips = [
     "Click on any AST node with a '+' to expand it",
 
     'Hovering over a node highlights the \
      corresponding part in the source code',
 
-    'Shift click on an AST node expands the whole substree'
+    'Shift click on an AST node expands the whole substree',
   ];
 
-  tips.forEach((tip, i) => console.log(`Tip ${i}` + tip));
+  tips.forEach((tip, i) => console.log(`Tip ${i}${tip}`));
 }
 
-const testeAsync = async value => {};
+const testeAsync = async (value) => {};
