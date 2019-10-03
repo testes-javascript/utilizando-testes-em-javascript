18241d7f327b1bdb894848923bb77248b33883e3
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2019-10-02 22:47:49 -0300

    Continuacao do capitulo5

---
+++ part-5/api/olaMundo.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function(req, res) {
+  res.send('Olá, mundo!');
+});
+
+app.get('/nome', function(req, res) {
+  res.send('Olá, leitor tudo bom?');
+});
+
+app.listen(3000, function() {
+  console.log('O App está em execução na porta: 3000!');
+});
