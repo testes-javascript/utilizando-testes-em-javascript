4b9565d25b10f89bfb6705b24371c6b784a3da1c
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2019-10-23 00:25:08 -0300

    Continuando o capitulo 5

---
+++ part-5/api/test/resources/user/user.test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('Suite de testes da API', () => {
+  it('Primeiro teste:', () => {
+    expect('teste').to.equals('teste');
+  });
+});
