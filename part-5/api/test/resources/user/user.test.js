7e3ec08ec0b942bd992bd4c5b7311ea5a87f8f15
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2019-10-25 00:40:42 -0300

    Continuando o capitulo 5

--- part-5/api/test/resources/user/user.test.js
+++ part-5/api/test/resources/user/user.test.js
@@ -1,9 +1,32 @@
-const chai = require('chai');
+import request from 'supertest';
+import chai from 'chai';
+
+import app from '../../../src/index';
 
 const { expect } = chai;
 
 describe('Suite de testes da API', () => {
-  it('Primeiro teste:', () => {
+  let expectedHello;
+
+  before(() => {
+    expectedHello = 'API ESTUDOS: Olá, mundo!';
+  });
+
+  it('Primeiro teste', () => {
     expect('teste').to.equals('teste');
+  });
+
+  it('[API] - Ola mundo', () => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end((err, { body: res }) => {
+        if (err) {
+          throw err;
+        } else {
+          const { message } = res;
+          expect(expectedHello).to.equals(message);
+        }
+      });
   });
 });
