3073a1e083f9b85a443f4b8b5d5e86fc34a31f7f
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2019-11-06 14:43:51 -0300

    Ajuste da estrutura

--- part-7/site-estudos/src/App.test.js
+++ part-7/site-estudos/src/App.test.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import { shallow } from 'enzyme';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('[FRONTEND][SHALLOW] - Teste logo', () => {
+  const wrapper = shallow(<App />);
+  // console.log(wrapper.debug());
+  const image = (
+    <img
+      src="https://cdn.worldvectorlogo.com/logos/react.svg"
+      width="30"
+      height="30"
+      className="d-inline-block align-top"
+      alt="React Bootstrap logo"
+    />
+  );
+  expect(wrapper.contains(image)).toEqual(true);
 });
