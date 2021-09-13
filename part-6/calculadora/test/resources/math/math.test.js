5e1189d125047ea278d823cbd8546b3c1f0276ae
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2021-09-13 19:22:02 -0300

    Ajustes

---
+++ part-6/calculadora/test/resources/math/math.test.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+const { expect } = chai;
+
+describe('Suite de testes da Cálculadora', () => {
+  it('Primeiro teste', () => {
+    expect('teste').to.equals('teste');
+  });
+});
