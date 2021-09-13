cb70bbb6a05b5e69e89a8bc3c614ac99b6e4bef1
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2021-09-12 22:51:25 -0300

    Ajustes

--- part-6/calculadora/src/controllers/MathController.js
+++ part-6/calculadora/src/controllers/MathController.js
@@ -1,6 +1,32 @@
 class MathController {
   math(req, res) {
-    return res.json(req.body);
+    const { dados, expressao } = req.body;
+
+    let value = 0;
+
+    switch (expressao) {
+      case 'soma':
+        value = dados.reduce((a, b) => parseInt(a) + parseInt(b));
+        break;
+
+      case 'subtracao':
+        value = dados.reduce((a, b) => parseInt(a) - parseInt(b));
+        break;
+
+      case 'divisao':
+        value = dados.reduce((a, b) => parseInt(a) / parseInt(b));
+        break;
+
+      case 'multiplicacao':
+        value = dados.reduce((a, b) => parseInt(a) * parseInt(b));
+        break;
+
+      default:
+        value = false;
+        break;
+    }
+
+    return res.json({ resultado: value });
   }
 }
 
