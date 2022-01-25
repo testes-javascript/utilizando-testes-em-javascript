a3da01888583e11e02e47cc8db544f5def6afc39
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2022-01-25 16:11:24 -0300

    Adjusment test

--- part-6/calculadora/test/resources/math/math.test.js
+++ part-6/calculadora/test/resources/math/math.test.js
@@ -2,6 +2,8 @@ import chai from 'chai';
 
 const { expect } = chai;
 
+import MathController from '../../../src/controllers/MathController';
+
 describe('Suite de testes da Cálculadora', () => {
   it('Primeiro teste', () => {
     expect('teste').to.equals('teste');
