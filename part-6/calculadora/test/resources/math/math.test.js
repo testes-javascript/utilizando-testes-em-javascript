252e80a43ddd6dcdde63e997fadf6a3cbfaa6894
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2022-01-27 22:20:29 -0300

    Adjustments of chapter

--- part-6/calculadora/test/resources/math/math.test.js
+++ part-6/calculadora/test/resources/math/math.test.js
@@ -1,11 +1,42 @@
+import request from 'supertest';
 import chai from 'chai';
+
+import app from '../../../src/index';
 
 const { expect } = chai;
 
 import MathController from '../../../src/controllers/MathController';
 
 describe('Suite de testes da Cálculadora', () => {
+  let expectedHello;
+
+  before(() => {
+    expectedHello = 'Calculadora: Olá, mundo!';
+  });
+
   it('Primeiro teste', () => {
     expect('teste').to.equals('teste');
+  });
+
+  it('[API] - Teste para garantir que a API subiu e está rodando', () => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end((err, { body: res }) => {
+        if (err) {
+          throw err;
+        } else {
+          const { message } = res;
+          expect(expectedHello).to.equals(message);
+        }
+      });
+  });
+
+  it('[RENDER] - Teste de renderização da cálculadora', done => {
+    request(app)
+      .get('/calculadora')
+      .expect('Content-type', /html/)
+      .expect(200)
+      .end(done);
   });
 });
